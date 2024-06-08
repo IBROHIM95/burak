5438c51149acc829c4465e065cc893dc1ea65941
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2024-06-09 04:02:09 +0600

    feat: craete route and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from  'express'
-import path from 'path'
+import path from 'path';
+import router from './router'
 
 // 1- qism entrance
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); //middle ware folder client(BROWSER)
+app.use(express.urlencoded({extended: true}));  //middleware : traditional API
+app.use(express.json()); //Middleware : Rest API
 
 // 2- qism SESSION
 
@@ -16,5 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
 // 4- ROUTERS
+
+app.use('/', router)   //middle ware 
 
 export default app
